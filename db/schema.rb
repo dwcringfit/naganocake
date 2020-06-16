5eff370138af54ba8bdfff58a91b01da5f04f9b6
vagrant <vagrant@localhost.localdomain>
2020-06-16 05:28:37 +0000

    [add]マイグレーションファイル追加
    item、genreのモデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_130233) do
+ActiveRecord::Schema.define(version: 2020_06_16_051551) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,24 @@ ActiveRecord::Schema.define(version: 2020_06_14_130233) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "is_valid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "context", null: false
+    t.string "image_id", null: false
+    t.integer "genre_id", null: false
+    t.integer "price", null: false
+    t.boolean "is_sale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
