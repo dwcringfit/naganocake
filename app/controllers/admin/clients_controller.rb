76eb460b83476cd84c007aa10e97bce4bbadc105
vagrant <vagrant@localhost.localdomain>
2020-06-16 15:27:58 +0000

    [Fix]admin/clients controller & index.html.erb

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -1,30 +1,30 @@
 class Admin::ClientsController < Admin::Base
-  before_action :authenticate_admin!
-
+before_action :set_client, only:[:show, :edit, :update]
   def index
     @clients = Client.page(params[:page]).per(10)
   end
 
   def show
-    @client = Client.find(params[:id])
   end
 
   def edit
-    @client = Client.find(params[:id])
   end
 
   def update
-    @client = Client.find(params[:id])
     if @client.update(client_params)
       redirect_to admin_client_path
     else
       flash[:client_updated_error] = "会員情報が正常に保存されませんでした。"
-      redirect_to edit_admin_client_path(@clients)
+      redirect_to edit_admin_client_path(@client)
     end
+  end
+
+  def set_client
+    @client = Client.find(params[:id])
   end
 
   private
   def client_params
-    params.require(:client).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :tel, :email, :is_valid)
+    params.require(:client).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_number, :address, :tel, :email, :is_valid)
   end
 end
