68d79ac31d2c624b00462d4cae77c6a3188b384d
vagrant <vagrant@localhost.localdomain>
2020-06-17 08:28:00 +0000

    [update]商品機能・画面、ジャンル機能・画面完成（レイアウト仮）
    一旦完成しました。見た目に関しては追々作業予定。

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -12,7 +12,7 @@ class Admin::GenresController < Admin::Base
 	     @genres = Genre.all
 	     redirect_to admin_genres_path
 	  else
-	     @genre = Genre.all
+	     @genres = Genre.all
 	     render 'index'
 	  end
   end
