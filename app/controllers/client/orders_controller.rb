1bac0860a7e65597f8df58ac16300d4624d7578d
vagrant <vagrant@localhost.localdomain>
2020-06-18 17:38:14 +0900

    nothing

---
+++ app/controllers/client/orders_controller.rb
@@ -0,0 +1,26 @@
+class Client::OrdersController < Client::Base
+
+    def thanks
+    end
+
+    def confirm
+
+    end
+
+    def index
+
+    end 
+
+    def new
+
+    end
+
+    def create
+    
+    end
+    
+    def show
+
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_051551) do
+ActiveRecord::Schema.define(version: 2020_06_18_060903) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -58,6 +58,19 @@ ActiveRecord::Schema.define(version: 2020_06_16_051551) do
     t.integer "genre_id", null: false
     t.integer "price", null: false
     t.boolean "is_sale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "status", null: false
+    t.integer "payment_method", null: false
+    t.integer "postage", null: false
+    t.integer "total_fee", null: false
+    t.string "address", null: false
+    t.string "post_number", null: false
+    t.string "receiver", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
