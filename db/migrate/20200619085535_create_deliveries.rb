aa1c5038dde84a15c86faf26cd6ce5e5637ddf3f
vagrant <vagrant@localhost.localdomain>
2020-06-20 05:23:39 +0000

    [Fix]client & delivery controller refactoling deliveryDB fix references from integer

--- db/migrate/20200619085535_create_deliveries.rb
+++ db/migrate/20200619085535_create_deliveries.rb
@@ -1,7 +1,7 @@
 class CreateDeliveries < ActiveRecord::Migration[5.2]
   def change
     create_table :deliveries do |t|
-      t.integer :client_id,   null: false, default: ""
+      t.references :client,   null: false, default: ""
       t.string :address,   null: false, default: ""
       t.string :post_number,   null: false, default: ""
       t.string :receiver,   null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_090954) do
+ActiveRecord::Schema.define(version: 2020_06_19_085535) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_06_18_090954) do
     t.string "receiver", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_deliveries_on_client_id"
   end
 
   create_table "genres", force: :cascade do |t|
