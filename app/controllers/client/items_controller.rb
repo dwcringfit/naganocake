d65d8f8e318cd6b15715f0e91bcafdcaeba22904
vagrant <vagrant@localhost.localdomain>
2020-06-22 04:38:19 +0000

    [Update] ヘッダー変更

--- app/controllers/client/items_controller.rb
+++ app/controllers/client/items_controller.rb
@@ -1,5 +1,5 @@
 class Client::ItemsController < Client::Base
-
+	skip_before_action  :authenticate_client!
 
   def index
        @genres = Genre.where(is_valid: true)
@@ -7,7 +7,7 @@ class Client::ItemsController < Client::Base
        @genre = Genre.find(params[:genre_id]) #該当のジャンルを探してくる
        @items = @genre.items.order(created_at: :desc).all #genre_idと紐づく商品を探してくる。且つ作成順に綺麗に並べる。
     else
-      @items = Item.all #それ以外は商品すべてを取得
+      @items = Item.where(genre_id: false) #それ以外は商品すべてを取得
     end
   end
 
