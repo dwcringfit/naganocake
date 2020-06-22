e11a36a71ff193113181ce584a523aaec009fba8
vagrant <vagrant@localhost.localdomain>
2020-06-22 20:31:10 +0900

    layout and clients can find only my show

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -1,13 +1,12 @@
 class Client::OrdersController < Client::Base
-  
-  def thanks
-  end
+  before_action :correct_client, only: [:show]
 
   def confirm
   end
 
   def index
-    @orders = Order.all
+    @orders = Order.where(client_id: current_client.id)
+    # @orders = Order.all
   end
 
   def new
@@ -21,5 +20,14 @@ class Client::OrdersController < Client::Base
     @order_items = @order.order_items
     @order_price = @order.order_items.find_by(order_id: params[:id])
   end
+
+private
+def correct_client
+    @order = Order.find(params[:id])
+    unless current_client.id == @order.client_id
+      redirect_to orders_path(current_client)
+      flash[:alert] = "アクセス権を超えています。あなたの注文履歴に戻りました。"
+    end
+end
 
 end
