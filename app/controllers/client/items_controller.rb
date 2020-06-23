ad16f6d86ebd6f6b0be98e213c2fe4bcaa320c94
vagrant <vagrant@localhost.localdomain>
2020-06-23 05:13:46 +0000

    [add]トップページ追加、レイアウト整理

--- app/controllers/client/items_controller.rb
+++ app/controllers/client/items_controller.rb
@@ -7,10 +7,9 @@ class Client::ItemsController < Client::Base
        @genre = Genre.find(params[:genre_id]) #該当のジャンルを探してくる
        @items = @genre.items.order(created_at: :desc).all #genre_idと紐づく商品を探してくる。且つ作成順に綺麗に並べる。
     else
-      @items = Item.where(genre_id: false) #それ以外は商品すべてを取得
+      @items = Item.all #それ以外は商品すべてを取得
     end
   end
-
 
   def show
     @item = Item.find(params[:id])
