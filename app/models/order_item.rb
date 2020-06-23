db439e64be067c5585f18acaf69450deeb21529a
vagrant <vagrant@localhost.localdomain>
2020-06-23 17:24:41 +0900

    enum

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,8 @@ class OrderItem < ApplicationRecord
 
     include CommonOrder
 
+    enum production_status: { unable: 0, wait_for_product: 1, in_production: 2, finish: 3 }
+
     belongs_to :order
     belongs_to :item
 
