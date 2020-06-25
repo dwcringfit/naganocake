bea5c10ab374e763fecae969eb24e8767b5ff075
vagrant <vagrant@localhost.localdomain>
2020-06-25 19:17:22 +0900

    aolmost complete

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,8 +2,18 @@ class Admin::OrderItemsController < ApplicationController
 
     def update
         @order_item = OrderItem.find(params[:id])
-        @order_item.update(order_item_params)
-        redirect_to admin_order_path(@order_item.order)
+        if @order_item.update_attributes(order_item_params)
+            if @order_item.in_production?
+                Order.where(id: @order_item.id).update_all(status: :in_production)
+            else @order_item.finish?
+                Order.where(id: @order_item.id).update_all(status: :shipped)
+            end
+            redirect_to admin_order_path(@order_item.order)
+        else
+            @order = Order.find(params[:id])
+            @order_items = @order.order_items
+            render template: 'admin/orders/show'
+        end
     end
 
     private
