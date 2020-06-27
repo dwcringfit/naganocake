8320b2b3e86e9e6458041315f189a8a234f886c5
vagrant <vagrant@localhost.localdomain>
2020-06-27 11:23:26 +0000

    [Update]一部レイアウト変更&TOPにオススメ表示

--- app/controllers/client/top_controller.rb
+++ app/controllers/client/top_controller.rb
@@ -5,7 +5,11 @@ class Client::TopController < Client::Base
        @genre = Genre.find(params[:genre_id]) #該当のジャンルを探してくる
        @items = @genre.items.order(created_at: :desc).all #genre_idと紐づく商品を探してくる。且つ作成順に綺麗に並べる。
     else
-      @items = Item.all #それ以外は商品すべてを取得
+       @items = Item.all #それ以外は商品すべてを取得
+    end
+    @items_recommend = Item.recommend_list
+    if @items_recommend.count == 0
+    	@items_recommend = Item.all.limit(4)
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,75 +7,75 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 管理者
-# Admin.create(
-#   email: 'test@test.com',
-#   password: 'password'
-# )
+ Admin.create(
+   email: 'test@test.com',
+   password: 'password'
+ )
 
-# #最初から追加されているジャンル名
-# [ 'ケーキ',
-# 	'プリン',
-# 	'焼き菓子',
-# 	'キャンディ'
-# ].each { |genre_name|
-# 	Genre.create( name: genre_name, is_valid: true)
-# }
+# 最初から追加されているジャンル名
+ [ 'ケーキ',
+ 	'プリン',
+ 	'焼き菓子',
+ 	'キャンディ'
+ ].each { |genre_name|
+ 	Genre.create( name: genre_name, is_valid: true)
+ }
 
-# 1..5.times do |i|
-#   Client.create(
-#   email: "test@test.com#{i}",
-#   password: "testtest",
-#   first_name: "花子#{i}",
-#   family_name: "山田#{i}",
-#   first_name_kana: "ハナコ",
-#   family_name_kana: "ヤマダ",
-#   post_number: "00000#{i}",
-#   address: "大阪府",
-#   tel: "909009#{i}"
-# )
-# end
+ 1..5.times do |i|
+   Client.create(
+   email: "test@test.com#{i}",
+   password: "testtest",
+   first_name: "花子#{i}",
+   family_name: "山田#{i}",
+   first_name_kana: "ハナコ",
+   family_name_kana: "ヤマダ",
+   post_number: "00000#{i}",
+   address: "大阪府",
+   tel: "909009#{i}"
+ )
+ end
 
-  # Item.create(
-  #   name: "cake",
-  #   context: "oisii",
-  #   price: 700,
-  #   genre_id: 2,
-  #   is_sale: true
-  # )
+   Item.create(
+     name: "cake",
+     context: "oisii",
+     price: 700,
+     genre_id: 2,
+     is_sale: true
+   )
 
-  # Order.create(
-  #   client_id: 1,
-  #   payment_method: 1,
-  #   postage: 800,
-  #   total_fee: 3000,
-  #   address: "京都府",
-  #   post_number: "0909009",
-  #   receiver: "田口"
-  # )
+   Order.create(
+     client_id: 1,
+     payment_method: 1,
+     postage: 800,
+     total_fee: 3000,
+     address: "京都府",
+     post_number: "0909009",
+     receiver: "田口"
+   )
 
-  # OrderItem.create(
-  #   order_id: 2,
-  #   item_id: 2,
-  #   item_count: 2,
-  #   price: 1100,
-  #   )
+   OrderItem.create(
+     order_id: 2,
+     item_id: 2,
+     item_count: 2,
+     price: 1100,
+     )
 
-# Client.create(
-# 	is_valid: "true",
-#     first_name: "清水",
-#     family_name: "光治",
-#     first_name_kana: "シミズ",
-#     family_name_kana: "ミツハル",
-#     tel: "00000000000",
-#     email: "m.s@mail",
-# 	password: "000000",
-# 	post_number: "0000000",
-# 	address: "大阪府大阪市北区",
-# )
+ Client.create(
+ 	is_valid: "true",
+     first_name: "清水",
+     family_name: "光治",
+     first_name_kana: "シミズ",
+     family_name_kana: "ミツハル",
+     tel: "00000000000",
+     email: "m.s@mail",
+ 	password: "000000",
+ 	post_number: "0000000",
+ 	address: "大阪府大阪市北区",
+ )
 
-# Delivery.create(
-#   client_id: 1,
-#   receiver: "清水光治",
-# 	post_number: "0000000",
-# 	address: "大阪府大阪市北区",
-# )
+ Delivery.create(
+   client_id: 1,
+   receiver: "清水光治",
+ 	post_number: "0000000",
+ 	address: "大阪府大阪市北区",
+ )
